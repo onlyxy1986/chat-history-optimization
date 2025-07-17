a000c19f2971285c680b081117614a7a8394bb15
only <onlyxuyang@foxmail.com>
2025-07-17 09:54:57 +0800

    更新 index.js

--- index.js
+++ index.js
@@ -311,7 +311,7 @@ globalThis.replaceChatHistoryWithDetails = async function (chat, contextSize, ab
     let tail = chat
         .slice(startIdx)
         .filter(item => item && item.is_user === false)
-        .map(item => item.replace(/<thinking>[\s\S])*?<\/thinking>/g,''));
+        .map(item => item.replace(/<thinking>[\s\S]*?<\/thinking>/g,''));
     mergedChat.push(...tail);
 
     chat[chat.length - 1]['mes'] = "用户输入:" + chat[chat.length - 1]['mes'] + "\n\n" + getCharPrompt();
